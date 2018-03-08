e9b26010546f8a0c871ad94a281332978981ba04
Ivan Procaccini <ivanprocaccini905@gmail.com>
2018-03-08 18:54:39 +0000

    code refactored

--- hangman.py
+++ hangman.py
@@ -13,56 +13,50 @@ class Hangman(object):
       lines = [l.rstrip() for l in f.readlines()]
       return [lines[i] for i in random.sample(range(len(lines)), 20)]
 
-
-  def run(self):
-    word = self.next_word()
-    so_far = ['_' for i in range(len(word))]
-    misses = []
-
-    while (not self.game_over(misses, so_far)):
-      self.print_stats(misses, so_far)
-      guess = self.get_guess() 
-      
-      if (not guess in word):
-        misses.append(guess)
-      else:
-        for i in range(len(word)):
-          if (word[i] is guess):
-            so_far[i] = guess
-    
-    if (len(misses) == 6):
-      print('Game Over! The word was \"' + word + '\".')
+  def play(self, guess):
+    if (not guess in self.word):
+      self.misses.append(guess)
     else:
-      print('You won!')
+      self.guessed.append(guess)
 
-  def game_over(self, misses, guessed):
-    return len(misses) == 6 or not '_' in guessed
+  def game_over(self):
+    return len(self.misses) == 6 or \
+        all([l in self.guessed for l in self.word]) 
 
   def next_word(self):
     self.next += 1
-    return self.dict[self.next]
+    self.misses = []
+    self.guessed = []
+    self.word = self.dict[self.next]
 
-  def print_stats(self, misses, guessed):
-    print('------------------------------')
-    print('Word: ' + ' '.join(guessed)) 
-    print('Misses: ' + str(misses))
 
-  def get_guess(self):
+# I/O utilities -------------------------------------------
+
+def end_game_msg(misses, word):
+  if (len(misses) == 6):
+    print('Game Over! The word was \"' + word + '\".')
+  else:
+    print('You won!\n\"' + word + '\"')
+
+def print_status(misses, guessed, word):
+  print('------------------------------')
+  print('Word: ' + ' '.join([c if c in guessed else '_' for c in word])) 
+  print('Misses: ' + str(misses))
+
+def get_guess():
+  guess = input('Guess: ')
+  while (not guess.isalpha()):
+    print('You can only play with letters, you dumbass!')
     guess = input('Guess: ')
-    while (not guess.isalpha()):
-      print('You can only play with letters, you dumbass!')
-      guess = input('Guess: ')
+  return guess
 
-    return guess
+@atexit.register
+def goodbye():
+  print('...Oh, nvm! Goodbye!')
 
-
+# Main ----------------------------------------------------
 
 if __name__ == '__main__':
-
-    @atexit.register
-    def goodbye():
-      print('...Oh, nvm! Goodbye!')
-    
     parser = argparse.ArgumentParser()
     parser.add_argument('--dict', 
         default='/usr/share/dict/words',
@@ -73,7 +67,13 @@ if __name__ == '__main__':
     game = Hangman(args.dict)
     while (True):
       try:
-        game.run()
+        game.next_word()
+        while (not game.game_over()):
+          print_status(game.misses, game.guessed, game.word)
+          guess = get_guess()
+          game.play(guess)
+        end_game_msg(game.misses, game.word)
+
         if (input('Play again? [y/n] ') in ['n', 'no', 'N', 'NO']):
           break
       except EOFError:
